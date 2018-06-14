aa74c6aac895dbe9e63a08c344f043ab33ae0e9a
fujisawa <a-fujisawa@digitalstage.jp>
2018-06-14 12:24:48 +0900

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const webpack = require('webpack');
-
-module.exports = [
-  {
-    entry: {
-      script: './src/app.ts'
-    },
-    output: {
-      path: __dirname + '/js',
-      filename: '[name].js'
-    },
-
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin()
-    ],
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: 'source-map',
-
-    devServer: {
-      contentBase: '',
-      inline: true,
-      hot: true,
-      port: 3000
-    },
-
-    resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: ['.ts', '.js']
-    },
-
-    module: {
-      loaders: [
-        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-        { test: /\.ts?$/, loader: 'ts-loader' }
-      ]
-    }
+module.exports = {
+  mode: "production",
+  entry: __dirname + "/src/app.ts",
+  output: {
+    path: __dirname + "/public",
+    filename: "app.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [
+          { loader: "ts-loader" }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  plugins: [],
+  performance: {
+    hints: false
+  },
+  devServer: {
+    contentBase: __dirname + "/public",
+    hot: true,
+    open: true
   }
-];
+};
